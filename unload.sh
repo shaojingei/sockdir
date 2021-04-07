79760a36d86664a06374a52590e6a9894df9387c
shaojingei <55829239+shaojingei@users.noreply.github.com>
2021-04-07 14:21:45 +0800

    Delete unload.sh

--- unload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-
-# Detach and unload the bpf_sockops_v4 program
-sudo bpftool cgroup detach "/sys/fs/cgroup/unified/" sock_ops pinned "/sys/fs/bpf/bpf_sockops"
-sudo rm "/sys/fs/bpf/bpf_sockops"
-
